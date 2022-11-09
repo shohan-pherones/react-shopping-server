23a50bf24bfe5d3d864c0c9ec1e46efcfd075774
shohan-pherones <shohan.sub.56@gmail.com>
2022-11-10 00:21:34 +0600

    server updated

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-const express = require("express");
-const cors = require("cors");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.port || 8000;
 
-const products = require("./products");
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Welcome to our React Shopping API");
-});
-
-app.get("/products", (req, res) => {
-  res.send(products);
-});
-
-const port = process.env.PORT || 8000;
-
-app.listen(port, console.log(`Server runningon port ${port}`));
+server.use(middlewares);
+server.use(router);
+server.listen(port);
